fb97108eb09bbc2c5867e735b376ca95ea7df1a8
Florent Biville <florent.biville@neo4j.com>
2021-05-31 12:44:31 +0200

    Assume Neo4j version 4 by default
    Fixes #37

--- movies.py
+++ movies.py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_url_path='/static/')
 url = os.getenv("NEO4J_URI", "neo4j+s://demo.neo4jlabs.com")
 username = os.getenv("NEO4J_USER", "movies")
 password = os.getenv("NEO4J_PASSWORD", "movies")
-neo4jVersion = os.getenv("NEO4J_VERSION", "")
+neo4jVersion = os.getenv("NEO4J_VERSION", "4")
 database = os.getenv("NEO4J_DATABASE", "movies")
 
 port = os.getenv("PORT", 8080)

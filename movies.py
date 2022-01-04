bb7c7413d6112272b7cbfaaf7ee91bc728bfdfe8
Robsdedude <rouven.bauer@neo4j.com>
2022-01-04 16:19:01 +0100

    Fix code style

--- movies.py
+++ movies.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
-import os
 from json import dumps
 import logging
+import os
 
-from flask import Flask, g, Response, request
-from neo4j import GraphDatabase, basic_auth
+from flask import (
+    Flask,
+    g,
+    request,
+    Response,
+)
+from neo4j import (
+    GraphDatabase,
+    basic_auth,
+)
+
 
 app = Flask(__name__, static_url_path="/static/")
 
 url = os.getenv("NEO4J_URI", "neo4j+s://demo.neo4jlabs.com")
 username = os.getenv("NEO4J_USER", "movies")
 password = os.getenv("NEO4J_PASSWORD", "movies")
-neo4jVersion = os.getenv("NEO4J_VERSION", "4")
+neo4j_version = os.getenv("NEO4J_VERSION", "4")
 database = os.getenv("NEO4J_DATABASE", "movies")
 
 port = os.getenv("PORT", 8080)
@@ -21,7 +30,7 @@ driver = GraphDatabase.driver(url, auth=basic_auth(username, password))
 
 def get_db():
     if not hasattr(g, "neo4j_db"):
-        if neo4jVersion.startswith("4"):
+        if neo4j_version.startswith("4"):
             g.neo4j_db = driver.session(database=database)
         else:
             g.neo4j_db = driver.session()
@@ -62,12 +71,16 @@ def serialize_cast(cast):
 
 @app.route("/graph")
 def get_graph():
+    def work(tx, limit):
+        return list(tx.run(
+            "MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) "
+            "RETURN m.title as movie, collect(a.name) as cast "
+            "LIMIT $limit",
+            {"limit": limit}
+        ))
+
     db = get_db()
-    results = db.read_transaction(lambda tx: list(tx.run("MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) "
-                                                         "RETURN m.title as movie, collect(a.name) as cast "
-                                                         "LIMIT $limit", {
-                                                             "limit": request.args.get("limit",
-                                                                                       100)})))
+    results = db.read_transaction(work, request.args.get("limit", 100))
     nodes = []
     rels = []
     i = 0
@@ -90,29 +103,42 @@ def get_graph():
 
 @app.route("/search")
 def get_search():
+    def work(tx, q_):
+        return list(tx.run(
+            "MATCH (movie:Movie) "
+            "WHERE movie.title =~ $title "
+            "RETURN movie",
+            {"title": "(?i).*" + q_ + ".*"}
+        ))
+
     try:
         q = request.args["q"]
     except KeyError:
         return []
     else:
         db = get_db()
-        results = db.read_transaction(lambda tx: list(tx.run("MATCH (movie:Movie) "
-                                                             "WHERE movie.title =~ $title "
-                                                             "RETURN movie", {"title": "(?i).*" + q + ".*"}
-                                                             )))
-        return Response(dumps([serialize_movie(record["movie"]) for record in results]),
-                        mimetype="application/json")
+        results = db.read_transaction(work, q)
+        return Response(
+            dumps([serialize_movie(record["movie"]) for record in results]),
+            mimetype="application/json"
+        )
 
 
 @app.route("/movie/<title>")
 def get_movie(title):
+    def work(tx, title_):
+        return tx.run(
+            "MATCH (movie:Movie {title:$title}) "
+            "OPTIONAL MATCH (movie)<-[r]-(person:Person) "
+            "RETURN movie.title as title,"
+            "COLLECT([person.name, "
+            "HEAD(SPLIT(TOLOWER(TYPE(r)), '_')), r.roles]) AS cast "
+            "LIMIT 1",
+            {"title": title_}
+        ).single()
+
     db = get_db()
-    result = db.read_transaction(lambda tx: tx.run("MATCH (movie:Movie {title:$title}) "
-                                                   "OPTIONAL MATCH (movie)<-[r]-(person:Person) "
-                                                   "RETURN movie.title as title,"
-                                                   "COLLECT([person.name, "
-                                                   "HEAD(SPLIT(TOLOWER(TYPE(r)), '_')), r.roles]) AS cast "
-                                                   "LIMIT 1", {"title": title}).single())
+    result = db.read_transaction(work, title)
 
     return Response(dumps({"title": result["title"],
                            "cast": [serialize_cast(member)
@@ -122,10 +148,17 @@ def get_movie(title):
 
 @app.route("/movie/<title>/vote", methods=["POST"])
 def vote_in_movie(title):
+    def work(tx, title_):
+        return tx.run(
+            "MATCH (m:Movie {title: $title}) "
+            "WITH m, (CASE WHEN exists(m.votes) THEN m.votes ELSE 0 END)"
+            " AS currentVotes "
+            "SET m.votes = currentVotes + 1;",
+            {"title": title_}
+        ).consume()
+
     db = get_db()
-    summary = db.write_transaction(lambda tx: tx.run("MATCH (m:Movie {title: $title}) "
-                                                    "WITH m, (CASE WHEN exists(m.votes) THEN m.votes ELSE 0 END) AS currentVotes "
-                                                    "SET m.votes = currentVotes + 1;", {"title": title}).consume())
+    summary = db.write_transaction(work, title)
     updates = summary.counters.properties_set
 
     db.close()
